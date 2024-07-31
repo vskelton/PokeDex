5f0aa04f656e4d6f2a9adfba97aabd7873869b91
vskelton <vskelton25@gmail.com>
2024-07-31 18:40:34 -0400

    Added Pokemon list to JS file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World');
-
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: '7', types: ['grass', 'poison']},
+    {name: 'Charmander', height: '6', types: 'fire'},
+    {name: 'Squirtle', height: '5', types: 'water'},
+];
