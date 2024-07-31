82b1d2e991c0e520f90e573e0e4ae85112d80465
vskelton <vskelton25@gmail.com>
2024-07-31 18:45:10 -0400

    Updated the height array to the correct number

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: '7', types: ['grass', 'poison']},
-    {name: 'Charmander', height: '6', types: 'fire'},
-    {name: 'Squirtle', height: '5', types: 'water'},
+    {name: 'Bulbasaur', height: '0.7', types: ['grass', 'poison']},
+    {name: 'Charmander', height: '0.6', types: 'fire'},
+    {name: 'Squirtle', height: '0.5', types: 'water'},
 ];
