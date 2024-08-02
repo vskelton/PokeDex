27384b511a38ac9cc1d80a491cec405e0963dbf1
vskelton <vskelton25@gmail.com>
2024-08-02 19:29:17 -0400

    Making changes to JS to so new pokemon list will be added

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ let pokemonList = [
     {name: 'Charmander', height: '0.6', types: 'fire'},
     {name: 'Squirtle', height: '0.5', types: 'water'},
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    console.log('name:', pokemon.name);
+    console.log('height:', pokemon.height);
+}
