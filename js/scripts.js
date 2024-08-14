2c7d7bb123d1e0c3051036c07a253dbc01a88688
vskelton <vskelton25@gmail.com>
2024-08-14 15:43:52 -0400

    Wrapped code in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: '0.7', types: ['grass', 'poison']},
-    {name: 'Charmander', height: '0.6', types: 'fire'},
-    {name: 'Squirtle', height: '0.5', types: 'water'},
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        {name: 'Bulbasaur', height: '0.7', types: ['grass', 'poison']},
+        {name: 'Charmander', height: '0.6', types: 'fire'},
+        {name: 'Squirtle', height: '0.5', types: 'water'},
 ];
 
-pokemonList.forEach(function(pokemon) {
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
 
-    console.log('name:', pokemon.name);
-    console.log('height:', pokemon.height);
+function getAll() {
+    return pokemonList;
+}
+
+return {
+    add: add,
+    getAll: getAll
+};
+})();
+
+let pokemons = pokemonRepository.getAll();
+
+pokemons.forEach(pokemon => { 
+
     document.write('<p>' + pokemon.name + ' | height: ' + pokemon.height + '</p>');
     if (pokemon.height <= 0.5) {
         document.write('Wow that is tiny!');
