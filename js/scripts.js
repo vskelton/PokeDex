ed6b3541198877920012cb7990b17b558f8ac676
vskelton <vskelton25@gmail.com>
2024-08-23 11:12:09 -0400

    created event listener

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,14 @@ let pokemonRepository = (function () {
 
 function add(pokemon) {
     if (
-        typeof pokemon === "object" &&
-        "name" in pokemon &&
-        "height" in pokemon &&
-        "types" in pokemon
+        typeof pokemon === 'object' &&
+        'name' in pokemon &&
+        'height' in pokemon &&
+        'types' in pokemon
     ) {
         repository.push(pokemon);
     } else {
-        console.log("pokemon is not correct");
+        console.log('pokemon is not correct');
     }
 }
 
@@ -23,13 +23,20 @@ function getAll() {
     return repository;
 }
 function addListItem(pokemon) {
-    let pokemonList = document.querySelector(".pokemon-list");
-    let listpokemon = document.createElement("li");
-    let button = document.createElement("button");
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement('button');
     button.innerText = pokemon.name;
-    button.classList.add("button-class");
+    button.classList.add('button-class');
+    button.addEventListener('click', function () {
+        showDetails(pokemon);
+    });
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
+}
+
+function showDetails(pokemon) {
+    console.log(pokemon.name);
 }
 
 return {
