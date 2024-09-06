d5ce853892cd0b9402c3b7bcf0d819d0a9e9f4ac
vskelton <vskelton25@gmail.com>
2024-09-06 11:36:50 -0400

    updated css file to add modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 let pokemonRepository = (function () {
+
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let modalContainer = document.querySelector('#modal-container');
   
     function add(pokemon) {
       if (
